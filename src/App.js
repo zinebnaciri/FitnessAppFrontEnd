9c2ed81ea1a583f0b63313de5302f5a1b7f7450f
zineb <126409044+catmilkomg@users.noreply.github.com>
2023-12-20 17:14:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SignInSide from './components/auth/login';
+
+function App() {
+  return (
+    <div className="App">
+     <SignInSide/>
+    </div>
+  );
+}
+
+export default App;

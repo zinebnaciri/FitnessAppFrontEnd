b8096f2edd2a29faef445ea9e6881e6a85a6ba2d
w0ram <55700217+w0ram@users.noreply.github.com>
2023-12-30 17:18:42 +0100

    better code + diet component

--- src/components/Diet.js
+++ src/components/Diet.js
@@ -1,18 +1,49 @@
-// Diet.js
 import React from 'react';
 import Layout from './assets/appbar';
 import StickyFooter from './assets/footer';
 
-const Diet = () => {
+const DietSection = ({ title, imageSrc, description }) => {
   return (
-    <div>
-      <Layout>
-      {/* Diet content */}
-      <h1>Diet</h1>
-      <StickyFooter/>
-      </Layout>
+    <div className="w-auto h-auto bg-neutral-800 rounded-3xl text-neutral-300 p-6 flex flex-col items-start gap-4 hover:bg-gray-900 hover:shadow-2xl hover:shadow-orange-400 transition-shadow">
+      <img className="w-full h-64 object-cover rounded-2xl mb-4" src={imageSrc} alt={`${title} Diet`} />
+      <div className="max-w-full">
+        <p className="font-extrabold text-xl">{title}</p>
+        <p className="text-sm font-bold overflow-hidden overflow-y-auto max-h-32">{description}</p>
+      </div>
+      <button className="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
     </div>
   );
 };
 
-export default Diet;
+const Diets = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-3xl font-bold mb-4">Diets</h1>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '30px', height: '65vh' }}>
+          <DietSection
+            title="Low Carb"
+            imageSrc="images/lowcarb.png"
+            description="A diet that restricts the intake of carbohydrates. It emphasizes protein, healthy fats, and non-starchy vegetables."
+          />
+
+          <DietSection
+            title="Mediterranean"
+            imageSrc="images/mediterranean.jpg"
+            description="Inspired by the traditional dietary patterns of countries bordering the Mediterranean Sea. It includes fruits, vegetables, whole grains, fish, and olive oil."
+          />
+
+          <DietSection
+            title="Vegetarian"
+            imageSrc="images/vegetarian.jpg"
+            description="A diet that excludes meat and sometimes other animal products. It focuses on plant-based foods like fruits, vegetables, grains, and legumes."
+          />
+        </div>
+      </Layout>
+
+      <StickyFooter />
+    </div>
+  );
+};
+
+export default Diets;
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -2,44 +2,43 @@ import React from 'react';
 import Layout from './assets/appbar';
 import StickyFooter from './assets/footer';
 
+const WorkoutSection = ({ title, imageSrc, description }) => {
+  return (
+    <div className="w-auto h-auto bg-neutral-800 rounded-3xl text-neutral-300 p-6 flex flex-col items-start gap-4 hover:bg-gray-900 hover:shadow-2xl hover:shadow-orange-400 transition-shadow">
+      <img className="w-full h-64 object-cover rounded-2xl mb-4" src={imageSrc} alt={`${title} Workout`} />
+      <div className="max-w-full">
+        <p className="font-extrabold text-xl">{title}</p>
+        <p className="text-sm font-bold overflow-hidden overflow-y-auto max-h-32">{description}</p>
+      </div>
+      <button className="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
+    </div>
+  );
+};
 
 const Workouts = () => {
   return (
     <div>
       <Layout>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '30px', height: '65vh' }}>
+        <h1 className="text-3xl font-bold mb-4">Workouts</h1>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '30px', height: '65vh' }}>
+          <WorkoutSection
+            title="Easy"
+            imageSrc="images/easy.webp"
+            description="Involves intense physical activity that challenges your cardiovascular system, strength, endurance, or a combination of these. The specific type of hard workout can vary based on your fitness goals, preferences, and fitness level."
+          />
 
-  <div className="w-auto h-auto bg-neutral-800 rounded-3xl text-neutral-300 p-6 flex flex-col items-start gap-4 hover:bg-gray-900 hover:shadow-2xl hover:shadow-orange-400 transition-shadow">
-    <img className="w-full h-64 object-cover rounded-2xl mb-4" src="images/easy.webp" alt="Easy Workout" />
-    <div className="max-w-full">
-      <p className="font-extrabold text-xl">Easy</p>
-      <p className="text-sm font-bold overflow-hidden overflow-y-auto max-h-32">
-        Involves intense physical activity that challenges your cardiovascular system, strength, endurance, or a combination of these. The specific type of hard workout can vary based on your fitness goals, preferences, and fitness level.
-      </p>
-    </div>
-    <button className="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
-  </div>
+          <WorkoutSection
+            title="Medium"
+            imageSrc="images/medium.jpg"
+            description="Moderate-intensity exercise session that provides a good balance between challenging your fitness level and being sustainable for regular practice. Here's an example of a medium workout that includes a mix of cardiovascular exercise, strength training, and flexibility work."
+          />
 
-  <div className="w-auto h-auto bg-neutral-800 rounded-3xl text-neutral-300 p-6 flex flex-col items-start gap-4 hover:bg-gray-900 hover:shadow-2xl hover:shadow-orange-400 transition-shadow">
-  <img className="w-full h-64 object-cover rounded-2xl mb-4" src="images/medium.jpg" alt="Medium Workout" />
-    <div className="max-w-full">
-      <p className="font-extrabold text-xl">Medium</p>
-      <p className="text-sm font-bold overflow-hidden overflow-y-auto max-h-32">Moderate-intensity exercise session that provides a good balance between challenging your fitness level and being sustainable for regular practice. Here's an example of a medium workout that includes a mix of cardiovascular exercise, strength training, and flexibility work</p>
-    </div>
-    <button className="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
-  </div>
-
-  <div className="w-auto h-auto bg-neutral-800 rounded-3xl text-neutral-300 p-6 flex flex-col items-start gap-4 hover:bg-gray-900 hover:shadow-2xl hover:shadow-orange-400 transition-shadow">
-  <img className="w-full h-64 object-cover rounded-2xl mb-4" src="images/hard.webp" alt="Hard Workout" />
-    <div className="max-w-full">
-      <p className="font-extrabold text-xl">Hard</p>
-      <p className="text-sm font-bold overflow-hidden overflow-y-auto max-h-32">Involves intense physical activity that challenges your cardiovascular system, strength, endurance, or a combination of these. The specific type of hard workout can vary based on your fitness goals, preferences, and fitness level
-      </p>
-    </div>
-    <button className="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
-  </div>
-</div>
-
+          <WorkoutSection
+            title="Hard"
+            imageSrc="images/hard.webp"
+            description="Involves intense physical activity that challenges your cardiovascular system, strength, endurance, or a combination of these. The specific type of hard workout can vary based on your fitness goals, preferences, and fitness level."
+          />
+        </div>
       </Layout>
 
       <StickyFooter />
